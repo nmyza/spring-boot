7a5227cf05fc9fd6fd209c2551969347a7ba433d
Nazar Myza <nazar.myza@gmail.com>
2017-12-24 02:52:00 +0200

    API url fix

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -2,7 +2,7 @@ var app = angular.module("springDemo", []);
 
 app.controller('AppCtrl', function ($scope, $http) {
     $scope.websites = [];
-    $http.get('http://localhost:8787/api/providers').success(function(response) {
+    $http.get('/api/providers').success(function(response) {
         $scope.websites = response;
     });
 });
