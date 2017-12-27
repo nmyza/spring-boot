7018f5eb09fb03a6d0b3d8660b190d5fefa8948a
nmyza <nazar.myza@gmail.com>
2017-12-27 19:12:24 +0200

    Init data method for Db in Service MongoDB IP was changed Gitignore file.

--- src/main/java/edu/grabduck/service/ProviderService.java
+++ src/main/java/edu/grabduck/service/ProviderService.java
@@ -5,6 +5,8 @@ import edu.grabduck.persistence.StackOverflowWebsiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -19,5 +21,18 @@ public class ProviderService {
 
     public List<StackOverflowWebsite> getAll() {
         return repository.findAll();
+    }
+
+    private static List<StackOverflowWebsite> sites = Arrays.asList(
+            new StackOverflowWebsite("id1", "https://cdn3.iconfinder.com/data/icons/diagram_v2/PNG/32x32/diagram_v2-12.png", "http://google.com", "Title 1", "Description 1"),
+            new StackOverflowWebsite("id2", "https://cdn3.iconfinder.com/data/icons/diagram_v2/PNG/32x32/diagram_v2-17.png", "http://google.com", "Title 2", "Description 2"),
+            new StackOverflowWebsite("id3", "https://cdn3.iconfinder.com/data/icons/diagram_v2/PNG/32x32/diagram_v2-14.png", "http://google.com", "Title 3", "Description 3"),
+            new StackOverflowWebsite("id4", "https://cdn3.iconfinder.com/data/icons/diagram_v2/PNG/32x32/diagram_v2-21.png", "http://google.com", "Title 4", "Description 4"),
+            new StackOverflowWebsite("id5", "https://cdn3.iconfinder.com/data/icons/diagram_v2/PNG/32x32/diagram_v2-10.png", "http://google.com", "Title 5", "Description 5")
+    );
+
+    @PostConstruct
+    private void init() {
+       repository.save(sites);
     }
 }
